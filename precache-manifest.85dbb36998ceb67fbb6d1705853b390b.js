1aee1a54322176ef2a908c8a7badc84b4e81a6bb
Jithesh M <jitheshmjithooz@gmail.com>
2022-11-04 19:43:21 +0530

    Updates

---
+++ precache-manifest.85dbb36998ceb67fbb6d1705853b390b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae2d583e7b72a6f6ca58",
+    "url": "/todo/static/js/main.ae2d583e.chunk.js"
+  },
+  {
+    "revision": "07156ea31df38cf958d6",
+    "url": "/todo/static/js/1.07156ea3.chunk.js"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "e9f4841dc63793007cfb34ada02cea9b",
+    "url": "/todo/index.html"
+  }
+];
